818e4e2fefb3a9716a91ed9a1fe76c7f4a69bf98
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-10-25 17:10:39 +0200

    seeder completato

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+//model 
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -12,8 +15,25 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            ComicSeeder::class
-        ]);
+        Comic::truncate();
+
+        $comics = config('comics');
+
+        foreach ($comics as $singleComic){
+            $comic = new Comic();
+            $comic->title = $singleComic['title'];
+            $comic->description = $singleComic['description'];
+            $comic->thumb = $singleComic['thumb'];
+            $priceNumber = floatval(substr($singleComic['price'], 1));
+            $comic-> price = $priceNumber;
+            $comic->series = $singleComic['series'];
+            $comic->sale_date = $singleComic['sale_date'];
+            $comic->type = $singleComic['type'];
+            $jsonArtists = json_encode($singleComic['artists']);
+            $comic->artists = $jsonArtists;
+            $implodedWriters = implode('|', $singleComic['writers']);
+            $comic->writers = $implodedWriters;
+            $comic->save();
+       }
     }
 }
